48df07f7ea8de409a4afd61882d60e0131009f4d
Anton Eriksson <anton.eriksson@umu.se>
2018-07-30 10:35:57 +0200

    Add licence header

--- gcMapExplorer/lib/hicparser.py
+++ gcMapExplorer/lib/hicparser.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+#
+# This file is part of gcMapExplorer
+# Copyright (C) 2016-2018  Rajendra Kumar, Ludvig Lizana, Per Stenberg
+#
+# gcMapExplorer is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# gcMapExplorer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gcMapExplorer.  If not, see <http://www.gnu.org/licenses/>.
+#
+# =============================================================================
 """
 Fast parser for Hi-C contact map format described in:
 Juicer Provides a One-Click System for Analyzing Loop-Resolution Hi-C Experiments
